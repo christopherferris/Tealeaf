8b73a1e3619fcbd806aaa876895b339c7a3db696
Chris Ferris <arizonachris@gmail.com>
2015-04-20 16:51:27 -0700

    update calculator

--- lesson1/calculator.rb
+++ lesson1/calculator.rb
@@ -1,22 +1,22 @@
 def say(msg)
-  puts "=> " + msg
+  puts "=> #{msg}"
 end
 
-say "Enter number: 1)addition 2)subtraction 3)multiplication
-4)division"
+say "Enter number: 1)addition 2)subtraction 3)multiplication 4)division"
 choice = gets.chomp
 say "Enter a first number"
 num1 = gets.chomp
 say "Enter a second number"
 num2 = gets.chomp
 
-if choice.to_i == 1
+case choice.to_i
+when 1
   result = num1.to_i + num2.to_i
-elsif choice.to_i == 2
+when 2
   result = num1.to_i - num2.to_i
-elsif choice.to_i == 3
+when 3
   result = num1.to_i * num2.to_i
-elsif choice.to_i == 4
+when 4
   result = num1.to_f / num2.to_f
 end
 

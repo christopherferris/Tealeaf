78cb927c1f2046a9a792992cc32b7943631b3a5c
Chris Ferris <arizonachris@gmail.com>
2015-04-22 18:21:57 -0700

    Update rps file

--- lesson1/rps.rb
+++ lesson1/rps.rb
@@ -5,6 +5,23 @@
 GAME = {'p' => 'Paper', 'r' => 'Rock', 's' => 'Scissors'}
 player_score = 0
 computer_score = 0
+
+def winning_message(winning_choice)
+    case winning_choice
+    when 'r' then puts "Rock smashes Scissors"
+    when 'p' then puts "Paper wraps Rock"
+    when 's' then puts "Scissors cuts Paper"
+    end
+  end
+
+  def tie_message(tie_choice)
+    case tie_choice
+    when 'r' then puts "Rock vs. Rock"
+    when 'p' then puts "Paper vs. Paper"
+    when 's' then puts "Scissors vs. Scissors"
+    end
+  end
+
 loop do
   puts "Ready to play Rock Paper Scissors?"
   begin
@@ -14,51 +31,22 @@ loop do
 
   computer_choice = GAME.keys.sample
 
-  def winning_message(winning_choice)
-    case winning_choice
-    when 'r'
-      puts "Rock smashes Scissors"
-    when 'p'
-      puts "Paper wraps Rock"
-    when 's'
-      puts "Scissors cuts Paper"
-    end
-  end
-
-  def tie_message(tie_choice)
-    case tie_choice
-    when 'r'
-      puts "Rock vs. Rock"
-    when 'p'
-      puts "Paper vs. Paper"
-    when 's'
-      puts "Scissors vs. Scissors"
-    end
-  end
-
   if player_choice == 'p' && computer_choice == 'r' || player_choice == 'r' && computer_choice == 's' || 
     player_choice == 's' && computer_choice == 'p'
     winning_message(player_choice)
     puts "You win!"
     player_score += 1
-    puts "Score:"
-    puts "Player: #{player_score} Computer: #{computer_score}"
   elsif player_choice == computer_choice
     tie_message(player_choice)
     puts "A tie!"
-    puts "Score:"
-    puts "Player: #{player_score} Computer: #{computer_score}"
   else
     winning_message(computer_choice)
     puts "Computer wins!"
     computer_score += 1
-    puts "Score:"
-    puts "Player: #{player_score} Computer: #{computer_score}"
   end
-
+  puts "Score:"
+  puts "Player: #{player_score} Computer: #{computer_score}"
   puts "Do you want to play again? (Y / N)"
   play_again = gets.chomp.downcase
-  if play_again != 'y'
-    break
-  end
+  break unless play_again == 'y'
 end
